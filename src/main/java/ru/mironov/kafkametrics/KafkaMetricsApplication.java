57e427e5710bc27e8fa44bcf1960f83d486ed558
Alexey Mironov <alexeymironov1991@gmail.com>
2024-07-13 14:23:44 +0300

    init

---
+++ src/main/java/ru/mironov/kafkametrics/KafkaMetricsApplication.java
@@ -0,0 +1,13 @@
+package ru.mironov.kafkametrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaMetricsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaMetricsApplication.class, args);
+    }
+
+}
